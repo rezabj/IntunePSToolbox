de28bd9581c360f86bf08536bdd6f005e1dc0f4f
Jan Řežab <honza@rezab.eu>
2021-12-19 20:18:47 +0100

    [MOD] Function help

--- Public/Get-IPSTDeviceEnrollmentConfigurations.ps1
+++ Public/Get-IPSTDeviceEnrollmentConfigurations.ps1
@@ -1,4 +1,22 @@
 function Get-IPSTDeviceEnrollmentConfigurations {
+    <#
+  .SYNOPSIS
+    Get-IPSTDeviceEnrollmentConfigurations
+  .DESCRIPTION
+    
+  .PARAMETER DeviceEnrollmentConfigurationId
+    Specifi Device Enrollment Configuration ID for get specific policy.
+  .INPUTS
+    None
+  .OUTPUTS
+    None
+  .NOTES
+    Author:         Jan Řežab
+    GitHub:         https://github.com/rezabj/IntunePSToolbox
+    Blog:           https://www.rezab.eu
+  .EXAMPLE
+    PS> Get-IPSTDeviceEnrollmentConfigurations -DeviceEnrollmentConfigurationId 00000000-0000-0000-0000-000000000000
+  #>
   [CmdletBinding()]
   param (
       [Parameter()]
@@ -11,7 +29,7 @@ function Get-IPSTDeviceEnrollmentConfigurations {
   }
   if ($DeviceConfigurationId) {
     $Params += @{
-      "GraphUri" = 'https://graph.microsoft.com/' + $IPSTGraphApiEnv + $Resource + "/" + $DeviceId
+      "GraphUri" = 'https://graph.microsoft.com/' + $IPSTGraphApiEnv + $Resource + "/" + $DeviceEnrollmentConfigurationId
     }
   } else {
     $Params += @{
