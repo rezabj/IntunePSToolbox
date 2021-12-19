98d181de37e2f46086fa66255a18fade6568a71b
Jan Řežab <honza@rezab.eu>
2021-12-19 20:16:13 +0100

    [MOD] Funcion help

--- Public/Get-IPSTDeviceConfigurationPolicies.ps1
+++ Public/Get-IPSTDeviceConfigurationPolicies.ps1
@@ -1,4 +1,22 @@
 function Get-IPSTDeviceConfigurationPolicies {
+  <#
+  .SYNOPSIS
+    Get-IPSTDeviceConfigurationPolicies
+  .DESCRIPTION
+    
+  .PARAMETER DeviceConfigurationId
+    Specifi Device Configuration ID for get specific policy.
+  .INPUTS
+    None
+  .OUTPUTS
+    None
+  .NOTES
+    Author:         Jan Řežab
+    GitHub:         https://github.com/rezabj/IntunePSToolbox
+    Blog:           https://www.rezab.eu
+  .EXAMPLE
+    PS> Get-IPSTDeviceConfigurationPolicies -DeviceConfigurationId 00000000-0000-0000-0000-000000000000
+  #>
   [CmdletBinding()]
   param (
       [Parameter()]
