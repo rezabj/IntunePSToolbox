ef871ed46c1950d1e49ae5ce4d092da6692e931d
Jan Řežab <honza@rezab.eu>
2021-12-19 20:20:39 +0100

    [MOD] Function help

--- Public/Get-IPSTManagedDevice.ps1
+++ Public/Get-IPSTManagedDevice.ps1
@@ -1,4 +1,22 @@
 function Get-IPSTManagedDevice {
+  <#
+  .SYNOPSIS
+    Get Managed Device.
+  .DESCRIPTION
+    
+  .PARAMETER DeviceID
+    Specifi DeviceID for get specific device.
+  .INPUTS
+    None
+  .OUTPUTS
+    None
+  .NOTES
+    Author:         Jan Řežab
+    GitHub:         https://github.com/rezabj/IntunePSToolbox
+    Blog:           https://www.rezab.eu
+  .EXAMPLE
+    PS> Get-IPSTManagedDevice -DeviceID 00000000-0000-0000-0000-000000000000
+  #>
   [CmdletBinding()]
   param (
       [Parameter()]
