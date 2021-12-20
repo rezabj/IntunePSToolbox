1baf26ed4f75a3b0de078df36a8052dcd9c96707
Jan Řežab <honza@rezab.eu>
2021-12-20 09:50:37 +0100

    [MOD] Build directory

--- Scripts/BuildModule.ps1
+++ Scripts/BuildModule.ps1
@@ -1,4 +1,7 @@
 $BuilPath = "..\Build\IntunePSToolbox"
+if (!(Test-Path -Path $BuilPath -PathType Container)) {
+  New-Item -Path $BuilPath -ItemType Directory -Force
+}
 Copy-Item -Path ..\IntunePSToolbox.psd1 -Destination $BuilPath -Force
 Copy-Item -Path ..\IntunePSToolbox.psm1 -Destination $BuilPath -Force
 Copy-Item -Path ..\Private -Destination $BuilPath -Recurse -Force
